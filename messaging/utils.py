28f50dac9ce84af2692d6d72ecd05ab2c5db4b95
om <ojfmcnulty@gmail.com>
2023-12-17 05:25:49 +0000

    lint

--- messaging/utils.py
+++ messaging/utils.py
@@ -5,20 +5,20 @@ ZMQ_PROTOCOLS = {"SHARED_MEMORY": "inproc://",
                  "TCP": "tcp://"}
 
 def get_zmq_protocol():
-    default_zmq_protocol = "tcp://";
-    force_protocol = os.getenv("ZMQ_MESSAGING_PROTOCOL");
+    default_zmq_protocol = "tcp://"
+    force_protocol = os.getenv("ZMQ_MESSAGING_PROTOCOL")
     if force_protocol is not None:
         default_zmq_protocol = ZMQ_PROTOCOLS[force_protocol]
     return default_zmq_protocol
-  
+
 
 def get_address():
-    address = "127.0.0.1";
-    default_address = os.getenv("ZMQ_MESSAGING_ADDRESS");
+    address = "127.0.0.1"
+    default_address = os.getenv("ZMQ_MESSAGING_ADDRESS")
     if default_address is not None:
         address = default_address
-    return address;
+    return address
 
 def get_zmq_socket_path(endpoint):
     return get_zmq_protocol() + get_address() + ":" + endpoint
-    
+
--- services_yaml.py
+++ services_yaml.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     print(yaml.dump(dict(services={
         key: {
             'port': SERVICE_LIST[key].port,
-            'frequency': SERVICE_LIST[key].frequency, 
+            'frequency': SERVICE_LIST[key].frequency,
             'should_log': SERVICE_LIST[key].should_log
             } for key in SERVICE_LIST})))
