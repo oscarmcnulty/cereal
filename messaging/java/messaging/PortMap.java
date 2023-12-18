6dde80ebfc215b97d7cd6ccf3de3310a955d2b44
om <ojfmcnulty@gmail.com>
2023-12-17 17:02:45 -0800

    Use portmap in file instead of generating

--- messaging/java/messaging/PortMap.java
+++ messaging/java/messaging/PortMap.java
@@ -8,13 +8,6 @@ import org.yaml.snakeyaml.constructor.Constructor;
 
 public class PortMap{
     public Map<String, Service> services;
-    public static final int STARTING_PORT = 5000;
-    public static final int RESERVED_PORT = 8022;  // sshd
-
-    public static int newPort(int idx){
-        int port = idx + STARTING_PORT;
-        return port >= RESERVED_PORT ? port + 1 : port;
-    }
     public PortMap load(){
         Yaml yaml = new Yaml(new Constructor(PortMap.class));
         PortMap portmap;
@@ -24,12 +17,6 @@ public class PortMap{
             inputStream.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
-
-        int idx = 0;
-        for (String service : portmap.services.keySet()){
-            portmap.services.get(service).port = newPort(idx);
-            idx += 1;
         }
         return portmap;
     }
